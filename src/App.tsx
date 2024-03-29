3ee23f276b382748110b9abc46f5b332912321db
Laxman <lax.cha29@gmail.com>
2024-03-29 10:23:11 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "antd";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-700 text-blue-600">
+        Hello world!
+      </h1>
+      <Button type="primary">Button</Button>
+    </>
+  );
+}
+
+export default App;
